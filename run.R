f38c3545fbbfe747c461ee6afb30ff67c5a78578
lampk <phungkhanhlam@gmail.com>
2015-08-09 08:16:48 +0700

    require to import knitr

--- run.R
+++ run.R
@@ -1,5 +1,5 @@
 ## packages need to be import
-pkg <- c("survival", "glmnet", "penalized",
+pkg <- c("knitr", "survival", "glmnet", "penalized",
          "ncvreg", "mgcv", "rpart", "randomForest", "gbm", "glmulti", "caret", "ROCR", "c060")
 sapply(pkg, function(x) devtools::use_package(x))
 
